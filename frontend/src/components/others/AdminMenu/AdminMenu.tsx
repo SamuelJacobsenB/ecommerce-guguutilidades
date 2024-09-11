96b2709fe09b2ae632c8e0a53070b57ab8a0782e
Samuel Jacobsen Barcellos <rcbsjbarcellos@gmail.com>
2024-09-11 17:07:11 -0300

    Menu estilizado

--- frontend/src/components/others/AdminMenu/AdminMenu.tsx
+++ frontend/src/components/others/AdminMenu/AdminMenu.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 import Link from 'next/link';
 import Header from '@/components/layout/header/Header';
 import { SearchType } from '@/types/SearchType';
-import { IoMenu } from 'react-icons/io5';
+import { IoMenu, IoHome, IoAdd, IoPencil } from 'react-icons/io5';
 import './AdminMenu.css';
 
 const AdminMenu = (props: SearchType) => {
@@ -21,7 +21,17 @@ const AdminMenu = (props: SearchType) => {
           className="menu_admin_icon"
           onClick={() => setMenuVisible(!menuVisible)}
         />
-        <div className={menuVisible ? `admin_nav_links` : 'disable'}></div>
+        <div className={menuVisible ? `admin_nav_links` : 'disable'}>
+          <Link href={`/home`} className="admin_menu_link">
+            <IoHome className="admin_menu_icon_box" /> Principal
+          </Link>
+          <Link href={`/admin/create`} className="admin_menu_link">
+            <IoAdd className="admin_menu_icon_box" /> Cadastrar produto
+          </Link>
+          <Link href={`/admin/modify`} className="admin_menu_link">
+            <IoPencil className="admin_menu_icon_box" /> Modificar produtos
+          </Link>
+        </div>
       </nav>
     </div>
   );
