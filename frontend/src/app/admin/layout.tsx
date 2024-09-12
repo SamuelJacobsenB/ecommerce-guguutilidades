01b33d7572bc05a8762e370aef5f6a54d464e6d2
Samuel Jacobsen Barcellos <rcbsjbarcellos@gmail.com>
2024-09-12 12:17:01 -0300

    Editando página de modify

--- frontend/src/app/admin/layout.tsx
+++ frontend/src/app/admin/layout.tsx
@@ -1,9 +1,4 @@
 'use client';
-
-import AdminMenu from '@/components/others/AdminMenu/AdminMenu';
-import { useState, useEffect, useCallback } from 'react';
-import getAllProducts from '@/functions/getAllProducts';
-import { Product } from '@/types/ProductType';
 import './layout.css';
 
 export default function RootLayout({
@@ -11,25 +6,9 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const [fixedProducts, setFixedProducts] = useState<Product[]>([]);
-  const [products, setProducts] = useState<Product[]>([]);
-
-  const getProducts = useCallback(async () => {
-    const allProducts: Product[] | undefined = await getAllProducts();
-    if (allProducts) {
-      setFixedProducts(allProducts);
-      setProducts(allProducts);
-    }
-  }, []);
-
-  useEffect(() => {
-    getProducts();
-  }, [getProducts]);
-
   return (
     <html lang="pt-br">
       <body>
-        <AdminMenu fixedProducts={fixedProducts} setProducts={setProducts} />
         <div className="admin_content">{children}</div>
       </body>
     </html>
--- frontend/src/app/admin/modify/page.tsx
+++ frontend/src/app/admin/modify/page.tsx
@@ -1,7 +1,58 @@
-import React from 'react';
+'use client';
+
+import { useState, useEffect, useCallback } from 'react';
+import { useRouter } from 'next/navigation';
+import { IoPencil, IoTrash } from 'react-icons/io5';
+import getAllProducts from '@/functions/getAllProducts';
+import AdminMenu from '@/components/others/AdminMenu/AdminMenu';
+import LoadImage from '@/components/others/LoadImage/LoadImage';
+import Button from '@/components/others/Button/Button';
+import { Product } from '@/types/ProductType';
+import './page.css';
 
 const Modify = () => {
-  return <div>Modify</div>;
+  const router = useRouter();
+  const [fixedProducts, setFixedProducts] = useState<Product[]>([]);
+  const [products, setProducts] = useState<Product[]>([]);
+
+  const getProducts = useCallback(async () => {
+    const allProducts: Product[] | undefined = await getAllProducts();
+    if (allProducts) {
+      setFixedProducts(allProducts);
+      setProducts(allProducts);
+    }
+  }, []);
+
+  useEffect(() => {
+    getProducts();
+  }, [getProducts]);
+
+  return (
+    <div>
+      <AdminMenu fixedProducts={fixedProducts} setProducts={setProducts} />
+      <h1 className="modify_title">Modificar produtos:</h1>
+      <div className="admin_product_list">
+        {products.map((product: Product) => {
+          const { id, picture, name } = product;
+
+          return (
+            <div className="admin_product" key={Number(id)}>
+              <LoadImage src={picture} alt={name} width={180} height={260} />
+              <h3 className="admin_product_name">{name}</h3>
+              <div className="admin_product_btn_area">
+                <Button type="button" colorType="blue">
+                  <IoPencil /> Editar
+                </Button>
+                <Button type="button" colorType="red">
+                  <IoTrash /> Deletar
+                </Button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Modify;
